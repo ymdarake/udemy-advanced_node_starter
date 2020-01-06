1438d2bfe0eb238e13ca308cac6ec41ec75ca9fb
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-07 12:52:11 +1300

    [78]chromium termination: afterEach

--- tests/header.test.js
+++ tests/header.test.js
@@ -8,9 +8,11 @@ beforeEach(async () => {
 	await page.goto('localhost:3000');
 });
 
+afterEach(async () => {
+	await browser.close();
+});
+
 test('We can launch a browser', async () => {
-
-
 	const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
 	expect(text).toEqual('Blogster');
