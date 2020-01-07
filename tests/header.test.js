1d91d75261825ffe984c9c95abc40aeec21b482d
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-07 16:01:27 +1300

    [87]Generating Sessions and Signatures

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,8 +12,36 @@ afterEach(async () => {
 	await browser.close();
 });
 
-test('We can launch a browser', async () => {
+test('the header has the correct text', async () => {
 	const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
 	expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+	await page.click('.right a');
+
+	const url = await page.url();
+
+	expect(url).toMatch(/accounts\.google\.com/);
+});
+
+
+test('When signed in, shows logout button', () => {
+	const id = '5a85dc351cd843163727f63c';// mongo user id
+
+	const Buffer = require('safe-buffer').Buffer;
+	const sessionObject = {
+		passport: {
+			user: id
+		}
+	};
+	const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+	const Keygrip = require('keygrip');
+	const keys = require('../config/keys');
+	const keygrip = new Keygrip([keys.cookieKey]);
+	const sig = keygrip.sign('session=' + sessionString);
+
+	console.log(sessionString, sig);
 });
