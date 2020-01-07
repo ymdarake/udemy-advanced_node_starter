119ab23416c2370169b63ea06a009e2107e525bd
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-07 17:40:51 +1300

    [105]Simplify Puppeteer Interface

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -32,6 +32,10 @@ class CustomPage {
 		await this.page.waitFor('a[href="/auth/logout"]');
 	}
 
+	async getContentsOf (selector) {
+		return this.page.$eval(selector, el => el.innerHTML);
+	}
+
 }
 
 module.exports = CustomPage;
