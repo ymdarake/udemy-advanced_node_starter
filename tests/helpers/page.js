45fc6b8d91ba459621c71827ab51428d4c58a728
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-07 17:26:21 +1300

    [101]Proxy: Custom Puppeteer Page

---
+++ tests/helpers/page.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+class CustomPage {
+	static build () {
+		const browser = await puppeteer.launch({
+			headless: false
+		});
+
+		const page = await browser.newPage();
+		const customPage = new CustomPage(page);
+
+		return new Proxy(customPage, {
+			get: function (target, property) {
+				return customPage[property] || page[property] || browser[property];
+			}
+		});
+	}
+
+	constructor(page) {
+		this.page = page;
+	}
+
+}
+
+module.exports = CustomPage;
